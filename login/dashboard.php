31b0526b533123c51b5147be2a85cd42769fca70
Ryan Nicolau Lopes <61668167+ryannlopes@users.noreply.github.com>
2023-05-25 21:20:45 -0300

    Update dashboard.php

--- login/dashboard.php
+++ login/dashboard.php
@@ -7,9 +7,24 @@ if (!isset($_SESSION["username"])) {
     exit();
 }
 
+// Conectar ao banco de dados
+    $conn = new mysqli("localhost", "root", "", "mecanica");
+
+    // Verificar a conexão com o banco de dados
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
 // Exibir o nome de usuário na página de dashboard
 $username = $_SESSION["username"];
 $nome     = $_SESSION["nomeUser"];
+// Consulta SQL para verificar as credenciais do usuário
+     $query = "SELECT * FROM usuario WHERE username = '$username'";
+              $busca = mysqli_query($conn, $query);
+
+              while ($dados = mysqli_fetch_array($busca)) {
+                $id = $dados['idUser'];
+                $nome = $dados['nomeUser'];
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +33,7 @@ $nome     = $_SESSION["nomeUser"];
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>Bem-vindo, <?php echo $username ?>!</h1>
+    <h1>Bem-vindo, <?php echo $nome; } ?>!</h1>
 
     <p>Esta é a página de dashboard.</p>
 
